82e0505ae18d1829f60009b54a2760fc6a1041bc
Phoenix <phoenixmakins@hotmail.com>
2022-02-25 03:28:08 -0500

    Moving Bar
    can detect when it's in the green zone and hitting spacebar will return if your in the green zone or not.

--- Assets/Scripts/MovingBar.cs
+++ Assets/Scripts/MovingBar.cs
@@ -8,6 +8,7 @@ public class MovingBar : MonoBehaviour
     [SerializeField] float Speed;
 
     private Vector3 startingPosition;
+    private int unlocked = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +22,39 @@ public class MovingBar : MonoBehaviour
         Vector3 v = startingPosition;
         v.x = distanceToCover * Mathf.Sin(Time.time * Speed);
         transform.position = v;
+
+        Spacebar();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "UnlockBar")
+        {
+            unlocked = 1;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        unlocked = 2;
+    }
+
+    public void Spacebar()
+    {
+        if(unlocked == 1)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                print("space key was pressed inside green bar");
+            }
+        }
+
+        if(unlocked == 2)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                print("space key was pressed outside green bar");
+            }
+        }
     }
 }
